43b5fac22191f4faea6eac04cad2a9cbdb749bdf
Mike Fogel <mike@fogel.ca>
2010-09-08 17:53:00 -0700

    Rewrite to better OOP

--- linode-dns-manager-sorter.user.js
+++ linode-dns-manager-sorter.user.js
@@ -10,19 +10,13 @@
 
 /* set up my namespace */
 if (window.bullseyelabs === undefined) bullseyelabs = new Object();
+if (bullseyelabs.ldms === undefined) bullseyelabs.ldms = new Object();
 
-bullseyelabs.ldms = {
+/* a library of comparison funcs */
+bullseyelabs.ldms.cmp = {
 
-    /* constants */
-    table_id: 'tablekit-table-1',
-
-    /* local structure, state */
-    tbody_node: null,
-    tr_dict: new Object(),
-    tr_order: new Array(),
-
-    /* used as a compare func for ordered_array.sort(cmp_func) */
-    domain_cmp: function(dom1, dom2) {
+    /* cmpare domains for ordered_array.sort(cmp_func) */
+    domain: function(dom1, dom2) {
         /* parse a domain into an array of 'levels' */
         var domain_parse = function(dom) {
             var pieces = dom.split('.');
@@ -44,55 +38,152 @@ bullseyelabs.ldms = {
         return domain_cmp_recurse(d1p, d2p);
     },
 
+    /* cmpare ints for ordered_array.sort(cmp_func) */
+    int: function(n1, n2) {
+        var i1 = parseInt(n1);
+        var i2 = parseInt(n2);
+        if (i1 == i2) return 0;
+        return (i1 < i2 ? -1 : 1);
+    },
+
+    /* cmpare words for ordered_array.sort(cmp_func) */
+    alpha: function(a1, c2) {
+        if (a1 == a2) return 0;
+        return (a1 < a2 ? -1 : 1);
+    },
+
+    /* cmpare ipaddrs for ordered_array.sort(cmp_func) */
+    ipaddr: function(ip1, ip2) {
+        var p1 = ip1.split('.');
+        var p2 = ip2.split('.');
+        for (var i=0; i<4; i++) {
+            var r = this.int_cmp(p1[i], p2[i]);
+            if (r != 0) return r;
+        }
+        return 0;
+    },
+
+    /* cmpare email addrs for ordered_array.sortcmp_func) */
+    email: function(e1, e2) {
+        var p1 = e1.split('@');
+        var p2 = e2.split('@');
+        /* first compare the domains */
+        var r = this.domain_cmp(p1[1], p2[1]);
+        if (r != 0) return r;
+        /* 2nd go to the usernames */
+        return this.alpha_cmp(p1[0], p2[0]);
+    },
+};
+
+/* abstract parent object */
+bullseyelabs.ldms.tsorter = function(cmp_spec)  {
+    this.cmp_spec = cmp_spec;
+    this.tr_dict = new Object();
+    this.tr_order = new Array();
+    return this;
+},
+
+bullseyelabs.ldms.tsorter.prototype = {
     /* parse the html table node */
-    parse_table: function() {
+    parse_table: function() { throw "'parse_table' not impl"; },
+
+    /* do the sort */
+    sort_rows: function() {
+        var cmp_spec = this.cmp_spec;
+        var cmp_func = function(a, b) {
+            for (var i=0; i<cmp_spec.length; i++) {
+                var order_key = cmp_spec[i]['key'];
+                var cur_cmp_func = cmp_spec[i]['func'];
+                var r = cur_cmp_func(a[order_key], b[order_key]);
+                if (r != 0) return r;
+            }
+            return 0;
+        };
+        this.tr_order.sort(
+            function(a, b) { return cmp_func(a, b); }
+        );
+    },
+
+    /* redisplay the table to reflect internal data structs */
+    refresh_table: function() { throw "'refresh_table' not impl"; },
+
+
+    /* run the object */
+    run: function() {
+        this.parse_table();
+        this.sort_rows();
+        this.refresh_table();
+    },
+},
+
+/* type1 tables */
+bullseyelabs.ldms.tsorter_type1 = function(table_id, cmp_spec) {
+    var me = new bullseyelabs.ldms.tsorter(cmp_spec);
+    me.tbody_node = document.getElementById(table_id).tBodies[0];
+
+    me.parse_table = function() {
+        var get_domain =  function(tr) {
+            tr.td = tr.childNodes[1];
+            var dom = tr.td.childNodes[1].firstChild.firstChild.wholeText;
+            return dom;
+        };
+
         for (var i=0; i<this.tbody_node.childNodes.length; i++) {
             var elem = this.tbody_node.childNodes[i];
             if (elem.tagName == undefined ||
                 elem.tagName.toLowerCase() != 'tr') continue;
-            var dom = this.parse_row(elem);
-            this.tr_dict[dom] = elem;
-            this.tr_order.push(dom);
+                this.tr_dict[i] = elem;
+            var dom = get_domain(elem);
+            var tr_val = {
+                'domain': dom,
+                'dict_key': i,
+            };
+            this.tr_order.push(tr_val);
         }
-    },
+    };
 
-    /* parse an individual table node */
-    parse_row: function(tr) {
-        tr.td = tr.childNodes[1];
-        var dom = tr.td.childNodes[1].firstChild.firstChild.wholeText;
-        return dom;
-    },
-
-    /* sort the internal data structures */
-    sort_rows: function() {
-        this.tr_order.sort(
-            function(a, b) { return bullseyelabs.ldms.domain_cmp(a, b); });
-    },
-
-    /* redisplay the table to reflect internal data structs */
-    refresh_table: function() {
+    me.refresh_table = function() {
         while (this.tbody_node.firstChild)
             this.tbody_node.removeChild(this.tbody_node.firstChild);
 
         for (var i=0; i<this.tr_order.length; i++) {
             var classname = (i % 2 ? 'roweven' : 'rowodd');
 
-            var tr = this.tr_dict[this.tr_order[i]];
+            var dict_key = this.tr_order[i]['dict_key'];
+            var tr = this.tr_dict[dict_key];
             tr.td.style.textAlign = 'right';
             tr.className = classname;
             this.tbody_node.appendChild(tr);
         }
-    },
+    };
 
-    init: function() {
-        var table_node = document.getElementById(this.table_id);
-        this.tbody_node = table_node.tBodies[0];
-        this.parse_table();
-        this.sort_rows();
-        this.refresh_table();
-    },
+    return me;
+},
+
+/* type2 tables */
+bullseyelabs.ldms.tsorter_type2 = function() {
+    // TODO: this.
 };
 
+bullseyelabs.ldms.main = function() {
+    if (window.location.pathname.lastIndexOf('domain_view.cfm') == -1) {
+        /* we're on the DNS manager front page */
+        // TODO: move this info to a config area
+        var ts = new bullseyelabs.ldms.tsorter_type1(
+            'tablekit-table-1',
+            new Array(
+                {'key': 'domain', 'func': bullseyelabs.ldms.cmp.domain}
+            )
+        );
+        ts.run();
+    }
+    else {
+        /* we're on the DNS domain detail page */
+        alert('ur?');
+    }
+};
+
+// TODO: do a 'handleOnLoad' object
 window.addEventListener(
-    'load', function(evt) { bullseyelabs.ldms.init(); }, false);
+    'load', function(evt) { bullseyelabs.ldms.main(); }, false);
 
